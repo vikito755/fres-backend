38f5711bb6b671dbcc43c1dfc838b7d19c192544
vikito755 <35623614+vikito755@users.noreply.github.com>
2023-03-13 09:00:11 +0200

    Update db.ts
    Remove error from missing user database.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,7 +11,6 @@ const connection = createConnection({
 
 // Database tables:
 const MARKETING_EMAILS = 'marketing_emails';
-const USERS = 'users';
 
 export const instertMarketingEmail = async (email: string) => {
 
@@ -33,7 +32,7 @@ export const instertMarketingEmail = async (email: string) => {
 export function emailExists(email: string) {
 // Check if an email already exists in a database table.
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT COUNT(*) AS count FROM ${USERS} WHERE email = ?`, email, function (error, results, fields) {
+    connection.query(`SELECT COUNT(*) AS count FROM ${MARKETING_EMAILS} WHERE email = ?`, email, function (error, results, fields) {
       if (error) reject(error);
       const count = results[0].count;
       resolve(count > 0);
